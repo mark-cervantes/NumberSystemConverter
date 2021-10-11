1f775acba3faeb71531d00014171ad6169a51767
Mark Cervantes <cmarkalfonso@gmail.com>
2021-10-11 17:46:10 +0800

    Created build file

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mark/NumberSystemConverter
+# Build directory: /home/mark/NumberSystemConverter/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
