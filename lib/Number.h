4b60e0a4e2630193eff94515c3a8e853964444dc
Mark Cervantes <cmarkalfonso@gmail.com>
2021-10-11 21:19:26 +0800

    completed Converter Declaration

--- lib/Number.h
+++ lib/Number.h
@@ -8,11 +8,10 @@ class Number
     public:
         Number (int base, std::string value) : _base(base), _value(value)
         { 
-            if (base == 2 ) {
-                _binary_value = _value;
-            }
             if (base == 10) {
-                _decimal_value = _value;
+                _decimal_value = std::stoll(_value;
+            } else {
+                _decimal_value = Number::Converter::any_to_decimal(base, value);
             }
         }
 
@@ -21,22 +20,20 @@ class Number
             Number(base, std::to_string(value));
         }
 
-        std::string binary();
-        std::string decimal();
-        std::string octal();
-        std::string hex();
+        std::string binary(int spacing = 4, std::string delimiter = " ");
+        std::string decimal(int spacing = 3, std::string delimiter = ",");
+        std::string octal(int spacing = 3, std::string delimiter = " ");
+        std::string hex(int spacing = 2, std::string delimiter = " ");
 
         /**
          * Eto yung low level necessary conversions na kailangan
          */
-        class Converter
+        struct Converter
         {
-            static std::string binary_to_hex(std::string binary_value);
-            static std::string binary_to_octal(std::string binary_value);
-            static std::string any_to_binary(int base, std::string value);
+            //static long long binary_to_decimal(std::string binary_value);
+            static std::string decimal_to_any(int base, long long decimal_value);
             static std::string binary_to_any(int base, std::string binary_value);
-            static std::string any_to_decimal(int base, std::string value);
-            static std::string decimal_to_any(int base, std::string decimal_value);
+            static long long any_to_decimal(int base, std::string value);
 
             // per character functions
             static int from_hex_char(char hex_char);
@@ -51,12 +48,12 @@ class Number
     private:
         /** 
          * optimizer
-         * serves as cache kasi maraming conversions ang manggagaling sa binary
-         * ex. imbis na decimal -> binary -> hex/oct
-         * magigi na lang siyang _binary_value -> hex/oct
+         * serves as cache kasi maraming conversions ang manggagaling sa decimal
+         * ex. imbis na oct -> decimal -> hex
+         * magigi na lang siyang _decimal_value -> hex nalang siya.
          */
-        std::string _binary_value;
-        std::string _decimal_value;
+        
+        long long _decimal_value;
 };
 
 #endif
--- src/Converter.cpp
+++ src/Converter.cpp
@@ -1,36 +1,65 @@
 #include "../lib/Number.h"
+#include <cmath>
+#include <cctype>
+#include <algorithm>
 
-std::string Number::Converter::binary_to_hex(std::string binary_value)
+long long Number::Converter::binary_to_decimal(std::string binary_value)
 {
-
+    long long decimal_value = 0;
+    for (int i = binary_value.length() - 1; i >= 0; --i) {
+        decimal_value += pow( (binary_value[i] - binary_value.length() - 1), (binary_value[i] - '0') );
+    }
+    return decimal_value;
 }
 
-std::string binary_to_octal(std::string binary_value)
+std::string Number::Converter::decimal_to_any(int base, long long decimal_value) 
 {
-
+    std::string result;
+    while (decimal_value > 0) {
+        result.insert(result.begin(), decimal_value % base);
+        decimal_value /= base;
+    }
+    return result;
 }
 
-std::string Number::Converter::any_to_binary(int base, std::string value)
+std::string Number::Converter::binary_to_any(int base, std::string binary_value)
 {
+    int cut;
+    switch(base) {
+        case 8: cut = 3; break; // octal
+        case 16: cut = 4; break; // hex
+        case 2: return binary_value; // binary
+        case 10: return std::to_string(binary_to_decimal(binary_value)); // decimal
+        default: cut = 1;
+    }
+    
+    // clean string from whitespaces
+    std::remove_if(binary_value.begin(), binary_value.end(), [] (char c) { return isspace(c); } );
 
+    // fill start of string with '0' until divisible by "cut" (octal is by 3, hex is by 4)
+    while (binary_value.length() % cut != 0) {
+        binary_value.insert(binary_value.begin(), '0');
+    }
+
+    std::string result;
+    for (int i = binary_value.length() - 1; i >= 0; i -= cut) {
+        std::string temp_value;
+        std::copy(binary_value.begin() + i - cut, binary_value.begin() + i, temp_value.begin());
+        result.insert(result.begin(), to_hex(binary_to_decimal(temp_value)));
+    }
+    return result;
 }
 
-std::string binary_to_any(int base, std::string binary_value)
+long long Number::Converter::any_to_decimal(int base, std::string value)
 {
-
+    long long decimal_result = 0;
+    for (int i = value.length() - 1; i >= 0; --i) {
+        decimal_result += (value[i] - '0') * pow(base, i - value.length() - 1);
+    }
+    return decimal_result;
 }
 
-std::string any_to_decimal(int base, std::string value)
-{
-
-}
-
-std::string decimal_to_any(int base, std::string decimal_value)
-{
-
-}
-
-int from_hex_char(char hex_char)
+int Number::Converter::from_hex_char(char hex_char)
 {
     if (std::isalpha(hex_char)) {
         if (hex_char < 'a') {
@@ -42,7 +71,7 @@ int from_hex_char(char hex_char)
     return hex_char - '0';
 }
 
-char to_hex(int value, bool is_capital = true)
+char Number::Converter::to_hex(int value, bool is_capital = true)
 {
     if (value >= 10 && value <= 15) {
         char hex = is_capital ? 'A' : 'a';
@@ -51,7 +80,7 @@ char to_hex(int value, bool is_capital = true)
     return value;
 }
 
-char to_hex(std::string value, bool is_capital = true)
+char Number::Converter::to_hex(std::string value, bool is_capital = true)
 {
     return to_hex(std::stoi(value), is_capital);
 }
